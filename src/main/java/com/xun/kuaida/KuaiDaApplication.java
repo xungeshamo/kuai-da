79d6e8d9b117c9b1184b6f7911a5069fa7c8a27d
xungeshamo <18635544209@163.com>
2025-09-21 22:07:06 +0800

    ``` feat(user): 初始化用户模块及相关功能
    新增用户实体、控制器、服务及数据传输对象，实现用户注册、登录、权限校验等基础功能。
    配置 MyBatis-Flex 代码生成器与相关依赖，集成 AOP 权限拦截注解 @AuthCheck。
    添加 MySQL 数据源配置及用户表创建 SQL 脚本，支持用户数据持久化。
    引入 JSON 配置解决 Long 类型精度丢失问题，完善用户角色枚举和常量定义。
    ```

--- src/main/java/com/xun/kuaida/KuaiDaApplication.java
+++ src/main/java/com/xun/kuaida/KuaiDaApplication.java
@@ -1,9 +1,11 @@
 package com.xun.kuaida;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xun.kuaida.mapper")
 public class KuaiDaApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/xun/kuaida/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.xun.kuaida.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.xun.kuaida.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
