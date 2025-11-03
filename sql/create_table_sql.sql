76c4d94a85058dc4aaa31605fa5d3215c622b836
xungeshamo <18635544209@163.com>
2025-11-03 21:58:46 +0800

    feat(sql): 添加应用表结构定义

--- sql/create_table_sql.sql
+++ sql/create_table_sql.sql
@@ -32,3 +32,34 @@ create table chat_history
     INDEX idx_createTime (createTime),             -- 提升基于时间的查询性能
     INDEX idx_appId_createTime (appId, createTime) -- 游标查询核心索引
 ) comment '对话历史' collate = utf8mb4_unicode_ci;
+
+
+-- 应用表
+-- auto-generated definition
+create table app
+(
+    id           bigint auto_increment comment 'id'
+        primary key,
+    appName      varchar(256)                       null comment '应用名称',
+    cover        varchar(512)                       null comment '应用封面',
+    initPrompt   text                               null comment '应用初始化的 prompt',
+    codeGenType  varchar(64)                        null comment '代码生成类型（枚举）',
+    deployKey    varchar(64)                        null comment '部署标识',
+    deployedTime datetime                           null comment '部署时间',
+    priority     int      default 0                 not null comment '优先级',
+    userId       bigint                             not null comment '创建用户id',
+    editTime     datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    createTime   datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime   datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint  default 0                 not null comment '是否删除',
+    constraint uk_deployKey
+        unique (deployKey)
+)
+    comment '应用' engine = InnoDB
+                   collate = utf8mb4_unicode_ci;
+
+create index idx_appName
+    on app (appName);
+
+create index idx_userId
+    on app (userId);
